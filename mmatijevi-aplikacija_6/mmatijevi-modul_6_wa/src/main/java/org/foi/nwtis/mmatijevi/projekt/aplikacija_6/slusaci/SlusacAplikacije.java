d5197c61aa36aaca8daeebd0f96bd5f4c5f6c8ba
mislavmatijevic <mislav.mim@gmail.com>
2022-07-05 11:38:16 +0200

    [A6] Implementirana pohrana JMS poruka.

--- mmatijevi-aplikacija_6/mmatijevi-modul_6_wa/src/main/java/org/foi/nwtis/mmatijevi/projekt/aplikacija_6/slusaci/SlusacAplikacije.java
+++ mmatijevi-aplikacija_6/mmatijevi-modul_6_wa/src/main/java/org/foi/nwtis/mmatijevi/projekt/aplikacija_6/slusaci/SlusacAplikacije.java
@@ -1,14 +1,22 @@
 package org.foi.nwtis.mmatijevi.projekt.aplikacija_6.slusaci;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.foi.nwtis.mmatijevi.projekt.aplikacija_6.modeli.JmsPoruka;
+import org.foi.nwtis.mmatijevi.projekt.aplikacija_6.zrna.jms.JmsPorukeZrno;
 import org.foi.nwtis.mmatijevi.projekt.ispis.Terminal;
 import org.foi.nwtis.mmatijevi.projekt.konfiguracije.Konfiguracija;
 import org.foi.nwtis.mmatijevi.projekt.konfiguracije.KonfiguracijaApstraktna;
 import org.foi.nwtis.mmatijevi.projekt.konfiguracije.NeispravnaKonfiguracija;
 
+import jakarta.ejb.EJB;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -16,6 +24,9 @@ import jakarta.servlet.annotation.WebListener;
 
 @WebListener
 public class SlusacAplikacije implements ServletContextListener {
+	@EJB
+	JmsPorukeZrno jmsPorukeZrno;
+	Konfiguracija konfig;
 
 	/**
 	 * Učitavanje konfiguracije
@@ -28,10 +39,10 @@ public class SlusacAplikacije implements ServletContextListener {
 		String putanja = kontekst.getRealPath("/WEB-INF") + File.separator;
 		nazivDatoteke = putanja + nazivDatoteke;
 
-		Konfiguracija konfig;
 		try {
 			konfig = KonfiguracijaApstraktna.preuzmiKonfiguraciju(nazivDatoteke);
 			kontekst.setAttribute("postavke", konfig);
+			procitajJmsPorukeIzDatoteke(konfig.dajPostavku("datoteka.jmsPoruke"));
 		} catch (NeispravnaKonfiguracija ex) {
 			Logger.getLogger(SlusacAplikacije.class.getName()).log(Level.WARNING, null, ex);
 			Terminal.greskaIspis("Konfiguracija nije ispravna! " + ex.getMessage());
@@ -46,9 +57,38 @@ public class SlusacAplikacije implements ServletContextListener {
 	 */
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		ServletContext context = sce.getServletContext();
-		context.removeAttribute("postavke");
+		pohraniJmsPorukeUDatoteku(konfig.dajPostavku("datoteka.jmsPoruke"));
+		ServletContext kontekst = sce.getServletContext();
+		kontekst.removeAttribute("postavke");
 		Terminal.infoIspis("Obrisani parametri konfiguracije!");
 		ServletContextListener.super.contextDestroyed(sce);
+	}
+
+	private void procitajJmsPorukeIzDatoteke(String nazivDatotekeJmsPoruka) {
+		try (FileInputStream fo = new FileInputStream(new File(nazivDatotekeJmsPoruka));
+				ObjectInputStream oo = new ObjectInputStream(fo);) {
+			jmsPorukeZrno.setListaPoruka(((LinkedList<JmsPoruka>) oo.readObject()));
+		} catch (Exception ex) {
+			Logger.getLogger(SlusacAplikacije.class.getName()).log(Level.SEVERE, "Problem s učitavanjem jms poruka",
+					ex);
+			Terminal.greskaIspis("JMS poruke nisu učitane!");
+		}
+	}
+
+	private void pohraniJmsPorukeUDatoteku(String nazivDatotekeJmsPoruka) {
+		File datoteka = new File(nazivDatotekeJmsPoruka);
+
+		if (!jmsPorukeZrno.getListaPoruka().isEmpty()) {
+			try (FileOutputStream fo = new FileOutputStream(datoteka);
+					ObjectOutputStream oo = new ObjectOutputStream(fo);) {
+				oo.writeObject(jmsPorukeZrno.getListaPoruka());
+			} catch (Exception ex) {
+				Logger.getLogger(SlusacAplikacije.class.getName()).log(Level.SEVERE, "Problem s pohranom jms poruka",
+						ex);
+				Terminal.greskaIspis("JMS poruke nisu pohranjene!");
+			}
+		} else if (datoteka.exists()) {
+			datoteka.delete();
+		}
 	}
 }
